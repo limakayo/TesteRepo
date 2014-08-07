ee00cba561214ed03231aeec026a4dc20d66c2b6
Kayo <lima.kayo@gmail.com>
2014-08-07 20:14:18 -0300

    Fixed the case on initial greeting display

--- Git/src/Main.java
+++ Git/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 	
 	public static void main(String[]args) {
+		System.out.println("This app shows sales data");
+		
 		System.out.println("Hello Git!");
 		System.out.println("New Push");
 		System.out.println("New Push");
