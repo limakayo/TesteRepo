c1db275c39681e1bfc2e11ad9c0ca44e2b206683
Kayo <lima.kayo@gmail.com>
2014-08-07 20:19:40 -0300

    Completed displayin data in a Sales

--- Git/src/Sales.java
+++ Git/src/Sales.java
@@ -3,8 +3,9 @@ public class Sales {
 	int data[] = {1,2,3};
 	
 	public void display() {
+		System.out.println("Data: ");
 		for(int i =0; i< data.length;i++) {
-			System.out.println("");
+			System.out.println("next value: " + data[i]);
 		}
 	}
 
