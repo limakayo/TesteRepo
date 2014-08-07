7d800bc8aa2522b2bcd65d74fcb89eca28e245ab
Kayo <lima.kayo@gmail.com>
2014-08-07 20:10:50 -0300

    Partial implementation of diplaying the numbers.

--- Git/src/Main.java
+++ Git/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 	
 	public static void main(String[]args) {
+		Sales data = new Sales();
+		data.display();
+		
+		
 		System.out.println("Hello Git!");
 		System.out.println("New Push");
 		System.out.println("New Push");
---
+++ Git/src/Sales.java
@@ -0,0 +1,11 @@
+
+public class Sales {
+	int data[] = {1,2,3};
+	
+	public void display() {
+		for(int i =0; i< data.length;i++) {
+			System.out.println("");
+		}
+	}
+
+}
